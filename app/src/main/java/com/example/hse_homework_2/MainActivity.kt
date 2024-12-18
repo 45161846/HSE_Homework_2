f56dcdf5f2abce82c361f37a935fe9af5c99ac62
JIUHKK <1761556@gmail.com>
2024-12-18 20:21:43 +0300

    code clearance

--- app/src/main/java/com/example/hse_homework_2/MainActivity.kt
+++ app/src/main/java/com/example/hse_homework_2/MainActivity.kt
@@ -1,47 +1,23 @@
 package com.example.hse_homework_2
 
-import android.net.ConnectivityManager
 import android.os.Bundle
-import android.telecom.ConnectionService
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.tooling.preview.Preview
-import coil3.Bitmap
 import coil3.compose.AsyncImage
-import coil3.compose.LocalPlatformContext
-import coil3.compose.SubcomposeAsyncImage
-import coil3.compose.SubcomposeAsyncImageContent
-import coil3.compose.rememberAsyncImagePainter
 import coil3.request.ImageRequest
-import coil3.request.crossfade
 import com.example.hse_homework_2.ui.theme.HSE_Homework_2Theme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val serv: ConnectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-
-
 
         setContent {
             HSE_Homework_2Theme {
@@ -56,7 +32,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-const val url = "https://raw.githubusercontent.com/45161846/HSE_Homework_2/refs/heads/master/HOOOOOOOOG.png"
+const val url =
+    "https://raw.githubusercontent.com/45161846/HSE_Homework_2/refs/heads/master/HOOOOOOOOG.png"
 
 @Composable
 fun ImageScreen() {
